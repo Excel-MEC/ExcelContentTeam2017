b757da10240a6a190433374f10d8ca062e2be2a6
aravi123 <aravindcranjan54@gmail.com>
2017-08-11 23:50:21 +0530

    Schedule added

--- Public/js/newEvent.js
+++ Public/js/newEvent.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
 			if ($(this).val()!="") {
 				rules.push($(this).val());
 			}
-		})
+		});
+		console.log($('.d1').val())
+
 		var id = $('input[name=name]').val().split(' ').join('-');
 		var data = {
 			id:id,
@@ -15,6 +17,8 @@ $(document).ready(function () {
 			FirstPrize:$('input[name=Prize_money1]').val(),
 			SecondPrize:$('input[name=Prize_money2]').val(),
 			ThirdPrize:$('input[name=Prize_money3]').val(),
+			fromDate:$('.d1').val(),
+			toDate:$('.d2').val(),
 			Rules:rules,
 			ContactNames:[
 				$('input[name=Contact_Name1]').val(),$('input[name=Contact_Name2').val()
@@ -26,7 +30,7 @@ $(document).ready(function () {
 		console.log(data);
 		if (window.location.pathname.startsWith("/editEvents")) {
 			$.post('/edit',data,function (response) {
-				console.log(response)
+				console.log(response);
 				if (response=="Sucess") {
 					// window.setTimeout(function () {
 					// 	window.location = "/"
@@ -45,26 +49,28 @@ $(document).ready(function () {
 	if (window.location.pathname.startsWith("/editEvents")) {
 		var url = window.location.href;
 		var eventid = url.split('?')[1].split("=")[1];
-		console.log(eventid)
+		console.log(eventid);
 		$.get('/events/'+eventid,function (response) {
 			console.log(response);
-			var html = ""
-			$('input[name=name]').val(response.name)
-			$('input[name=Prize_money1]').val(response.FirstPrize)
-			$('input[name=Prize_money2]').val(response.SecondPrize)
-			$('input[name=Prize_money3]').val(response.ThirdPrize)
-			$('input[name=Contact_Name1]').val(response.ContactNames[0])
-			$('input[name=Contact_Name2]').val(response.ContactNames[1])
-			$('input[name=Contact_Phone_number1]').val(response.ContactPhNos[0])
-			$('input[name=Contact_Phone_number2]').val(response.ContactPhNos[1])
-			$(".rules input").attr("type","hidden")
+			var html = "";
+			$('input[name=name]').val(response.name);
+			$('input[name=Prize_money1]').val(response.FirstPrize);
+			$('input[name=Prize_money2]').val(response.SecondPrize);
+			$('input[name=Prize_money3]').val(response.ThirdPrize);
+			$('input[name=Contact_Name1]').val(response.ContactNames[0]);
+			$('input[name=Contact_Name2]').val(response.ContactNames[1]);
+			$('input[name=Contact_Phone_number1]').val(response.ContactPhNos[0]);
+			$('input[name=Contact_Phone_number2]').val(response.ContactPhNos[1]);
+			$('.d1').val(response.fromDate);
+			$('.d2').val(response.toDate);
+			$(".rules input").attr("type","hidden");
 			for (var i = 0; i < response.Rules.length; i++) {
-				html = "<input type=text name=Rules value="+response.Rules[i]+"><br>"
+				html = "<input  class='form-control' type=text name=Rules value="+response.Rules[i]+"><br>"
 				$('.rules').append(html)
 			}
-			$('textarea').froalaEditor('html.set',response.description)
-			$('textarea1').froalaEditor('html.set',response.eventFormat)
+			$('textarea').froalaEditor('html.set',response.description);
+			$('textarea1').froalaEditor('html.set',response.eventFormat);
 		})
 		
 	}
-})
+});
